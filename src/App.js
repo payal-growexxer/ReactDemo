314a670f38d7a3a219a8fdf34133c82e55dd56e5
payal-growexxer <payal.chauhan@growexx.com>
2024-01-03 11:46:33 +0530

    Crud react demo

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
+import Create from "./components/Create";
+import Edit from "./components/Edit";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="geeks">ReactJs CRUD DEMO </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/create"
+            element={<Create />}
+          />
+          <Route
+            path="/edit"
+            element={<Edit />}
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
